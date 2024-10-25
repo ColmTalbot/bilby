7657a20fc315a38b6cc3d7538024a0b7474d2ce1
Colm Talbot <talbotcolm@gmail.com>
2024-10-25 17:24:06 -0500

    FEAT: support multiband and relative binning likelihoods

--- bilby/gw/likelihood/multiband.py
+++ bilby/gw/likelihood/multiband.py
@@ -3,6 +3,7 @@ import math
 import numbers
 
 import numpy as np
+from bilback.utils import array_module
 
 from .base import GravitationalWaveTransient
 from ...core.utils import (
@@ -715,32 +716,21 @@ class MBGravitationalWaveTransient(GravitationalWaveTransient):
             An object containing the SNR quantities.
 
         """
-        strain = np.zeros(len(self.banded_frequency_points), dtype=complex)
-        for mode in waveform_polarizations:
-            response = interferometer.antenna_response(
-                self.parameters['ra'], self.parameters['dec'],
-                self.parameters['geocent_time'], self.parameters['psi'],
-                mode
-            )
-            strain += waveform_polarizations[mode][self.unique_to_original_frequencies] * response
+        modes = {
+            mode: value[self.unique_to_original_frequencies]
+            for mode, value in waveform_polarizations.items()
+        }
+        strain = interferometer.get_detector_response(
+            modes, self.parameters, frequencies=self.banded_frequency_points
+        )
 
-        dt = interferometer.time_delay_from_geocenter(
-            self.parameters['ra'], self.parameters['dec'],
-            self.parameters['geocent_time'])
-        dt_geocent = self.parameters['geocent_time'] - interferometer.strain_data.start_time
-        ifo_time = dt_geocent + dt
+        d_inner_h = (strain @ self.linear_coeffs[interferometer.name]).conjugate()
 
-        calib_factor = interferometer.calibration_model.get_calibration_factor(
-            self.banded_frequency_points, prefix='recalib_{}_'.format(interferometer.name), **self.parameters)
-
-        strain *= np.exp(-1j * 2. * np.pi * self.banded_frequency_points * ifo_time)
-        strain *= calib_factor
-
-        d_inner_h = np.conj(np.dot(strain, self.linear_coeffs[interferometer.name]))
+        xp = array_module(strain)
 
         if self.linear_interpolation:
-            optimal_snr_squared = np.vdot(
-                np.real(strain * np.conjugate(strain)),
+            optimal_snr_squared = xp.vdot(
+                (strain * strain.conjugate()).real,
                 self.quadratic_coeffs[interferometer.name]
             )
         else:
@@ -750,18 +740,18 @@ class MBGravitationalWaveTransient(GravitationalWaveTransient):
                 start_idx, end_idx = self.start_end_idxs[b]
                 Mb = self.Mbs[b]
                 if b == 0:
-                    optimal_snr_squared += (4. / self.interferometers.duration) * np.vdot(
-                        np.real(strain[start_idx:end_idx + 1] * np.conjugate(strain[start_idx:end_idx + 1])),
+                    optimal_snr_squared += (4. / self.interferometers.duration) * xp.vdot(
+                        (strain[start_idx:end_idx + 1] * strain[start_idx:end_idx + 1].conjugate()).real,
                         interferometer.frequency_mask[Ks:Ke + 1] * self.windows[start_idx:end_idx + 1]
                         / interferometer.power_spectral_density_array[Ks:Ke + 1])
                 else:
                     self.wths[interferometer.name][b][Ks:Ke + 1] = (
                         self.square_root_windows[start_idx:end_idx + 1] * strain[start_idx:end_idx + 1]
                     )
-                    self.hbcs[interferometer.name][b][-Mb:] = np.fft.irfft(self.wths[interferometer.name][b])
-                    thbc = np.fft.rfft(self.hbcs[interferometer.name][b])
-                    optimal_snr_squared += (4. / self.Tbhats[b]) * np.vdot(
-                        np.real(thbc * np.conjugate(thbc)), self.Ibcs[interferometer.name][b])
+                    self.hbcs[interferometer.name][b][-Mb:] = xp.fft.irfft(self.wths[interferometer.name][b])
+                    thbc = xp.fft.rfft(self.hbcs[interferometer.name][b])
+                    optimal_snr_squared += (4. / self.Tbhats[b]) * xp.vdot(
+                        thbc * np.conjugate(thbc).real, self.Ibcs[interferometer.name][b])
 
         complex_matched_filter_snr = d_inner_h / (optimal_snr_squared**0.5)
 
